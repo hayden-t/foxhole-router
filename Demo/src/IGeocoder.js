54a35868d52264003062660b2be18caa571e8bf7
Derp <Derp>
2019-08-08 22:00:02 -0700

    * Demonstration update
    Added demonstration map
    Added towns.json file with location definitions
    Added compiled FoxholeRouter.js
    Updated readme with basic build instructions and use guide

---
+++ Demo/src/IGeocoder.js
@@ -0,0 +1,109 @@
+define(['leaflet', '../towns.json'], function (L, towns) {
+    return {
+        FoxholeGeocoder: function () {
+            var FoxholeGeocoder = {
+                /* distance between two strings */
+                levinshtein: function (a, b) {
+                    if (a.length == 0) return b.length;
+                    if (b.length == 0) return a.length;
+
+                    var matrix = [];
+
+                    // increment along the first column of each row
+                    var i;
+                    for (i = 0; i <= b.length; i++) {
+                        matrix[i] = [i];
+                    }
+
+                    // increment each column in the first row
+                    var j;
+                    for (j = 0; j <= a.length; j++) {
+                        matrix[0][j] = j;
+                    }
+
+                    // Fill in the rest of the matrix
+                    for (i = 1; i <= b.length; i++) {
+                        for (j = 1; j <= a.length; j++) {
+                            if (b.charAt(i - 1) == a.charAt(j - 1)) {
+                                matrix[i][j] = matrix[i - 1][j - 1];
+                            } else {
+                                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
+                                    Math.min(matrix[i][j - 1] + 1, // insertion
+                                        matrix[i - 1][j] + 1)); // deletion
+                            }
+                        }
+                    }
+
+                    return matrix[b.length][a.length];
+                },
+
+                /* The geocoder resolve function, name -> location */
+                geocode: function (q, callback, context) {
+                    var query = q.toLowerCase();
+                    var townkey = Object.keys(towns).find(key => key.toLowerCase() === query);
+                    if (townkey != null) {
+                        var town = towns[townkey];
+                        let call = callback.bind(context);
+                        var value = { center: L.latLng(town.y, town.x), name: query, bbox: L.latLngBounds(L.latLng(town.y, town.x), L.latLng(town.y, town.x)) };
+                        return call([value], []);
+                    }
+                    else {
+                        let call = callback.bind(context);
+                        return call([], []);
+                    }
+                },
+
+                /* The geocoding reverse lookup - nearest point */
+                reverse: function (location, scale, callback, context) {
+                    let call = callback.bind(context);
+                    var townlist = Object.keys(towns);
+                    if (townlist.length === 0)
+                        return call([], []);
+                    var distance = 10000000;
+                    var index = -1;
+                    for (var i = 0; i < townlist.length; i++) {
+                        var disty = (location.lat - towns[townlist[i]].y);
+                        var distx = (location.lng - towns[townlist[i]].x);
+                        var dist_squared = distx * distx + disty * disty;
+                        if (dist_squared < distance) {
+                            distance = dist_squared;
+                            index = i;
+                        }
+                    }
+                    if (index == -1)
+                        return call([], []);
+
+                    var town = towns[townlist[index]];
+                    var value = { center: L.latLng(town.y, town.x), name: townlist[index], bbox: L.latLngBounds(L.latLng(town.y, town.x), L.latLng(town.y, town.x)) }
+                    return call([value], []);
+                },
+
+                /* Auto-suggest using indexof and levinshtein distance */
+                suggest: function (q, callback, context) {
+                    var query = q.toLowerCase();
+                    let call = callback.bind(context);
+                    var townlist = Object.keys(towns);
+                    if (townlist.length === 0)
+                        return call([], []);
+                    var index = -1;
+                    var results = [];
+                    for (var i = 0; i < townlist.length; i++) {
+                        var townname = townlist[i].toLowerCase();
+                        if (townname.indexOf(query) >= 0) {
+                            var d = FoxholeGeocoder.levinshtein(query, townname);
+                            results.push({ name: townlist[i], distance: d });
+                        }
+                    }
+                    results.sort(x => x.distance);
+                    var output = [];
+                    for (var i = 0; i < 5 && i < results.length; i++) {
+                        var town = towns[results[i].name];
+                        output.push({ center: L.latLng(town.y, town.x), name: results[i].name, bbox: L.latLngBounds(L.latLng(town.y, town.x), L.latLng(town.y, town.x)) });
+                    }
+                    return call(output, []);
+                }
+            };
+            return FoxholeGeocoder;
+        }
+    };
+});
---
+++ Demo/src/IRouter.js
@@ -0,0 +1,90 @@
+define(['leaflet', 'json-loader!../../Mapped/Unified.fitted-for-leaflet.geojson', 'geojson-path-finder', '../towns.json', 'leaflet-routing-machine'], function (L, Paths, PathFinder, towns, routing_machine) {
+    return {
+        FoxholeRouter: function(mymap) {
+            var FoxholeRouter = {
+                //Roads: L.geoJSON(Paths),
+                Roads: L.geoJSON(Paths).addTo(mymap),
+                renderer: L.canvas().addTo(mymap),
+                NetworkLayer: L.layerGroup().addTo(mymap),
+                pathFinder: new PathFinder(Paths, { precision: 1e-1 }),
+                routeLine: function (route, options) {
+                    return new Line(route, options);
+                },
+                route: function (waypoints, callback, context, options) {
+                    // modify new waypoints to find closest ones
+                    for (var i = 0; i < waypoints.length; i++) {
+                        var closestPoint = null;
+                        var distance = 0.0;
+                        for (var key in FoxholeRouter.Roads._layers) {
+                            var layer = FoxholeRouter.Roads._layers[key];
+                            for (var k = 0; k < layer._latlngs.length; k++) {
+                                var lat = layer._latlngs[k].lat;
+                                var wplat = waypoints[i].latLng.lat;
+                                var lng = layer._latlngs[k].lng;
+                                var wplng = waypoints[i].latLng.lng;
+                                var distance_squared = (lat - wplat) * (lat - wplat) + (lng - wplng) * (lng - wplng);
+                                if (!closestPoint || distance_squared < distance) {
+                                    distance = distance_squared;
+                                    closestPoint = L.latLng(lat, lng);
+                                }
+                            }
+                        }
+                        waypoints[i].latLng = closestPoint;
+                    }
+
+                    var path = null;
+                    for (var i = 0; i < waypoints.length - 1; i++) {
+                        var start = waypoints[i].latLng;
+                        var finish = waypoints[i + 1].latLng;
+                        if (path == null)
+                            path = FoxholeRouter.pathFinder.findPath({ name: "path", geometry: { coordinates: [start.lng, start.lat] } }, { geometry: { coordinates: [finish.lng, finish.lat] } });
+                        else {
+                            var p = FoxholeRouter.pathFinder.findPath({ name: "path", geometry: { coordinates: [start.lng, start.lat] } }, { geometry: { coordinates: [finish.lng, finish.lat] } });
+                            if (p != null && p.path != null) {
+                                for (var k = 1; k < p.path.length; k++)
+                                    path.path.push(p.path[k]);
+                                path.weight += p.weight;
+                            }
+                        }
+                    }
+
+                    let call = callback.bind(FoxholeRouter);
+
+                    if (path === null)
+                        call({ status: -1, message: "Could not find a route" }, []);
+                    else {
+                        var coordinates = [];
+                        var instructions = [];
+                        for (var i = 0; i < path.path.length; i++) {
+                            coordinates[i] = L.latLng(path.path[i][0], path.path[i][1]);
+                            instructions[i] = "";
+                        }
+
+                        var result = call(null, [{
+                            name: "route name",
+                            summary:
+                            {
+                                totalTime: 0.0,
+                                totalDistance: path.weight
+                            },
+                            waypoints: waypoints,
+                            coordinates: coordinates,
+                            instructions: instructions
+                        }], context);
+                        FoxholeRouter.NetworkLayer.clearLayers()
+                        for (var i = 0; i < path.path.length - 1; i++) {
+                            new L.polyline(
+                                [
+                                    [path.path[i][1], path.path[i][0]],
+                                    [path.path[i + 1][1], path.path[i + 1][0]]
+                                ], { color: 'white', weight: 7, opacity: 1.0, renderer: FoxholeRouter.renderer, interactive: false, smoothFactor: 1 }
+                            ).addTo(FoxholeRouter.NetworkLayer).bringToFront();
+                        }
+                        return result;
+                    }
+                }
+            };
+            return FoxholeRouter;
+        }
+    };
+});
